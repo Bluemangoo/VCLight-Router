b179b57c6c38b50257623c538038a0ed201b03bb
bluemangoo <chenfy2006@qq.com>
2023-05-21 12:19:45 +0800

    Feat: export config interface

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export default VCLightRouter;
 export { default as RequestContext } from "./types/requestContext";
 export { default as ResponseContext } from "./types/responseContext";
 export { default as CookieElement } from "./types/cookieElement";
+export { default as VCLightRouterConfig } from "./types/vclightRouterConfig";
 export { default as buildInRouters } from "./buildInRouters";
